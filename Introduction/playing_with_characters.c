ef31dcaad4a243beb8083d14a44acf9f6ddf11d7
Zachary Perales <aardzarks@gmail.com>
2023-02-24 11:07:42 -0500

    Add missing newline characters

--- Introduction/playing_with_characters.c
+++ Introduction/playing_with_characters.c
@@ -13,10 +13,10 @@ int main()
     char* s = malloc(sizeof(char) * MAX_LEN);
     char* sen = malloc(sizeof(char) * MAX_LEN); 
     scanf("%c", &ch);
-    printf("%c", ch);   
+    printf("%c\n", ch);   
     scanf("\n");
     scanf("%[^\n]%*c", s);
-    printf("%s", s);
+    printf("%s\n", s);
     scanf("%[^\n]%*c", sen);
     printf("%s", sen);
     
